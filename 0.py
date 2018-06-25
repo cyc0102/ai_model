df79172521a089162f10a0ef05dea5a5211c3401
Bryan Chen <cyc0102@gmail.com>
2018-06-25 17:28:15 +0800

    fine tune and add ramom seed

--- 0.py
+++ 0.py
@@ -5,6 +5,7 @@ from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.utils import np_utils  # 用來後續將 label 標籤轉為 one-hot-encoding  
 from matplotlib import pyplot as plt
+np.random.seed(10)
 
 # 載入 MNIST 資料庫的訓練資料，並自動分為『訓練組』及『測試組』
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -32,7 +33,7 @@ x_Train_norm = X_train_2D/255
 x_Test_norm = X_test_2D/255
 
 # 進行訓練, 訓練過程會存在 train_history 變數中
-train_history = model.fit(x=x_Train_norm, y=y_TrainOneHot, validation_split=0.2, epochs=10, batch_size=800, verbose=2)  
+train_history = model.fit(x=x_Train_norm, y=y_TrainOneHot, validation_split=0.2, epochs=10, batch_size=200, verbose=2)  
 
 # 顯示訓練成果(分數)
 scores = model.evaluate(x_Test_norm, y_TestOneHot)  
